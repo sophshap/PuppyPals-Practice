c1c5ad688725b98cf087a65c905648cf908eaa77
sophshap <sophieashapiro@gmail.com>
2023-10-26 10:33:57 -0400

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+
+
+function App() {
+
+
+
+  return (
+    <>
+      <div>
+
+      </div>
+
+    </>
+  )
+}
+
+export default App
---
+++ src/main.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App.jsx'
+import './index.css'
+
+
+// importing whole app component wand rendering to the DOM
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+)
