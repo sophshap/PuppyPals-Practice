e45f7c6ec017e21e40d6ea34ee07d42c6cf3e84b
sophshap <sophieashapiro@gmail.com>
2023-10-27 16:13:50 -0400

    more content puppy details

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,34 @@
-
+import { puppyList } from './data.js'
+import { useState } from "react"
+import "./App.css"
 
 function App() {
+  const [puppies, setPuppies] = useState(puppyList)
+  const [clickedPuppy, setClickedPuppy] = useState(null)
 
-
+  function handleClick(puppy) {
+    console.log(puppy)
+    setClickedPuppy(puppy)
+  }
 
   return (
     <>
       <div>
+        {
+          puppies.map((puppy) => {
+            return <p key={puppy.id} onClick={() => handleClick(puppy)}>{puppy.name}</p>
+          })
+        }
+        {clickedPuppy && <div className="details">
+          <h1>{clickedPuppy.name}</h1>
+          <p>{clickedPuppy.age}</p>
 
+          <ul>
+            {clickedPuppy.tricks.map((trick) => {
+              return <li key={trick.id}>trick: {trick.title}!!</li>
+            })}
+          </ul>
+        </div>}
       </div>
 
     </>
--- src/main.jsx
+++ src/main.jsx
@@ -7,7 +7,7 @@ import './index.css'
 // importing whole app component wand rendering to the DOM
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+
+  <App />
+
 )
